c2d53d3abadf530d129ffcc4871bbd3095860bb9
bbeckford305 <bridgette.beckford@gmail.com>
2021-04-23 09:25:59 -0700

    Completed garments curl script

--- curl-scripts/garments/update.sh
+++ curl-scripts/garments/update.sh
@@ -3,19 +3,15 @@
 API="https://virtual-closet-api.herokuapp.com"
 URL_PATH="/garments"
 
-curl "${API}${URL_PATH}" \
+curl "${API}${URL_PATH}/${ID}" \
   --include \
-  --request POST \
+  --request PATCH \
   --header "Content-Type: application/json" \
-  --header "Authorization: Bearer ${TOKEN}" \
-  --data '{
+--header "Authorization: Bearer ${TOKEN}" \
+--data '{
     "garment": {
-      "type": "'"${TYPE}"'",
-      "color": "'"${COLOR}"'"
-      "cleaningMethod": "'"${CMETHOD}"'"
       "cleaningStatus": "'"${CSTATUS}"'"
       "wornDate": "'"${WDATE}"'"
-      "weather": "'"${WEATHER}"'"
     }
   }'
 
---
+++ curl-scripts/garments/view.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+API="https://virtual-closet-api.herokuapp.com"
+URL_PATH="/garments"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request GET \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+      "garment": {
+        "type": "'"${TYPE}"'",
+        "color": "'"${COLOR}"'"
+        "weather": "'"${WEATHER}"'"
+      }
+    }'
+
+echo
